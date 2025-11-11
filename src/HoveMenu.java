19dae0d89f61b2a68c0779c5c75e66e86f81c1b8
saravvy <ajpwsaya@gmail.com>
2025-11-11 13:41:04 +0100

    case 2 hoveMenu in progress. Genre

--- src/HoveMenu.java
+++ src/HoveMenu.java
@@ -12,32 +12,32 @@ public class HoveMenu {
 
         System.out.println("                Main Menu         ");
         System.out.println("Choose an option:");
-        System.out.println("1. Show all movies");
-        System.out.println("2. Search for a movie");
+        System.out.println("1. Show all movies & Search ");
+        System.out.println("2. Search for a genre");
 
     }
 
     public void switchChoice(FileIO io) {
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        String choice = input.nextLine();
+        String choice = scan.nextLine();
 
         boolean found = false;
+        FilmList filmList = new FilmList();
+        filmList.addFilm(io);
+        ArrayList<Film> films = filmList.getFilms();
         switch (choice) {
             case "1":
-                FilmList filmList = new FilmList();
-                filmList.addFilm(io);
 
-                ArrayList<Film> films = filmList.getFilms();
+
                 for (Film film : films) {
                     System.out.println(film);
                 }
                 while (found == false) {
 
 
-
                     System.out.println("Which movie do you wanna watch?");
-                    Scanner scan = new Scanner(System.in);
+
                     String choicedMovie = scan.nextLine().toLowerCase();
 
 
@@ -59,11 +59,50 @@ public class HoveMenu {
                 break;
 
             case "2":
-                break;
+
+                while (found == false) {
+
+
+                    System.out.println("Which genre do you wanna watch?");
+
+                    String choicedGenre = scan.nextLine().toLowerCase();
+
+
+                    for (Film b : films) {
+                        if (b.getGenre().toLowerCase().trim().equals(choicedGenre.trim())) {
+                            if (!films.isEmpty()) {
+                                ArrayList<String> genres = new ArrayList<>();
+
+                                for (Film o : films) {
+                                    String[] filmGenres = b.getGenre().split(",");
+                                    for (String g : filmGenres) {
+                                        genres.add(g.trim().toLowerCase());
+
+                                        for (String u : genres) {
+                                            System.out.println(g);
+                                            found = true;
+
+                                            break;
+                                        }
+                                    }
+                                    if (!found) {
+                                        System.out.println("Film not found. Try again");
+
+                                    }
+                                }
+
+
+
+                            }
+
+                        }
+                    }
+                } break;
 
             default:
+        }
 
         }
     }
-}
+
 
